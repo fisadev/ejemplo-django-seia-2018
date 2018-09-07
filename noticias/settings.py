2c1b21bbcb8e43d332b14e1d3543169178ee2b0a
Juan Pedro Fisanotti <fisadev@gmail.com>
2018-09-06 21:18:42 -0300

    Lets try

--- noticias/settings.py
+++ noticias/settings.py
@@ -77,8 +77,8 @@ WSGI_APPLICATION = 'noticias.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'noticias',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -123,3 +123,7 @@ STATIC_URL = '/static/'
 
 
 django_heroku.settings(locals())
+import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config(default=DATABASE_URL)
+}
